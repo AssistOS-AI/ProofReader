0e909fbb0b40491468daed19e720bcac4b2db8c9
Koroem <conduratvictor@yahoo.com>
2024-01-10 11:04:31 +0200

    updates

--- services/routingService.js
+++ services/routingService.js
@@ -3,8 +3,13 @@ export class routingService {
     async navigateToLocation(locationArray = [], appName) {
         const PROOFREADER_PAGE = "proofreader-page";
 
-        if (locationArray.length === 0 || locationArray[0] !== PROOFREADER_PAGE) {
-            console.error("Invalid URL structure: URL must start with 'proofreader-page'");
+       if (locationArray.length === 0 || locationArray[0] === PROOFREADER_PAGE) {
+            const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${PROOFREADER_PAGE}`;
+            await webSkel.changeToDynamicPage(PROOFREADER_PAGE, pageUrl);
+            return;
+        }
+         if(locationArray[locationArray.length-1]!== PROOFREADER_PAGE){
+         console.error(`Invalid URL: URL must end with ${PROOFREADER_PAGE}`);
             return;
         }
         const webComponentName = locationArray[locationArray.length - 1];

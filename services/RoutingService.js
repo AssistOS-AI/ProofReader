28ceab4a444fe214ab65a7c3f41a178375904606
alupoaiemircea <mircea@axiologic.net>
2024-03-04 16:53:11 +0200

    refactor classes to pascal case

--- services/RoutingService.js
+++ services/RoutingService.js
@@ -1,4 +1,4 @@
-export class routingService {
+export class RoutingService {
     constructor() {}
     async navigateToLocation(locationArray = [], appName) {
         const PROOFREADER_PAGE = "proofreader-page";

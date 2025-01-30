8ae668cc6bebd4bffa2e225d7fbcac89e9f9dea9
alupoaiemircea <mircea@axiologic.net>
2025-01-30 15:46:12 +0200

    move translate as a plugin

---
+++ web-components/translate-document-modal/translate-document-modal.js
@@ -0,0 +1,57 @@
+const llmModule = require("assistos").loadModule("llm", {});
+const personalityModule = require("assistos").loadModule("personality", {});
+const documentModule = require("assistos").loadModule("document", {});
+import pluginUtils from "../../../../../../wallet/core/plugins/pluginUtils.js";
+
+export class TranslateDocumentModal{
+    constructor(element, invalidate) {
+        this.invalidate = invalidate;
+        this.element = element;
+        let context = pluginUtils.getContext(this.element);
+        this.documentId = context.documentId;
+        this.invalidate(async ()=>{
+            this.personalities = await personalityModule.getPersonalities(assistOS.space.id);
+        });
+    }
+    beforeRender() {
+        let personalitiesHTML = "";
+        for (let personality of this.personalities) {
+            personalitiesHTML += `<option value="${personality.id}">${personality.name}</option>`;
+        }
+        this.personalitiesOptions = personalitiesHTML;
+    }
+    afterRender(){
+        let personalitySelect = this.element.querySelector("#personality");
+        let audioLanguages;
+        personalitySelect.addEventListener("change", async (e)=>{
+            let value = e.target.value;
+            let languageSelect = this.element.querySelector("#language");
+            languageSelect.innerHTML = "";
+            if(value !== ""){
+                languageSelect.removeAttribute("disabled");
+                let personality = this.personalities.find((p)=> p.id === value);
+                let textLanguages = await llmModule.getModelLanguages(assistOS.space.id, personality.llms["text"]);
+                if(textLanguages.length === 0){
+                    languageSelect.innerHTML = `<option value="" disabled selected>No languages available</option>`;
+                    languageSelect.setAttribute("disabled", "true");
+                    return;
+                }
+                for(let language of textLanguages){
+                    languageSelect.innerHTML += `<option value="${language}">${language}</option>`;
+                }
+            }
+        });
+
+    }
+
+    async translateDocument(targetElement){
+        let formData = await assistOS.UI.extractFormInformation(targetElement);
+        if(formData.isValid){
+            let taskId = await documentModule.translateDocument(assistOS.space.id, this.documentId, formData.data.language, formData.data.personality);
+            assistOS.UI.closeModal(this.element);
+        }
+    }
+    closeModal(){
+       assistOS.UI.closeModal(this.element);
+    }
+}
